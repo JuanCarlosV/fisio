13bb1cfa5227465361e811697cfc68388485ac7e
JuanCarlosV <juan.carlos.villegas@icloud.com>
2024-04-19 02:32:46 +0000

    Refactor PhysiotherapistsController to use Model in update method

--- src/main/java/com/fisioapp/calendar/controllers/PhysiotherapistsController.java
+++ src/main/java/com/fisioapp/calendar/controllers/PhysiotherapistsController.java
@@ -5,26 +5,39 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.fisioapp.calendar.models.Physiotherapist;
 import com.fisioapp.calendar.services.PhysiotherapistsService;
 
 @Controller
-@RequestMapping("/physiotherapists")
+@RequestMapping("/physios")
 public class PhysiotherapistsController {
 
     @Autowired
     private PhysiotherapistsService physiotherapistService;
 
-    @PostMapping
-    public void addPhysiotherapist(@RequestBody Physiotherapist physiotherapist) {
+    @GetMapping("/add")
+    public String addPhysiotherapist(Model model) {
+        model.addAttribute("physiotherapist", new Physiotherapist());
+        model.addAttribute("title", "Nuevo Alumno");
+        return "/views/physios/add";
+    }
+
+    @PostMapping("/create")
+    public String createPhysiotherapist(@ModelAttribute Physiotherapist physiotherapist, BindingResult result) {
+        if (result.hasErrors()) {
+            return "physios/create";
+        }
+
         physiotherapistService.create(physiotherapist);
+        return "redirect:/physios/";
     }
 
     @PostMapping("/{id}")
@@ -34,7 +47,7 @@ public class PhysiotherapistsController {
 
         // trabajo en progreso..
 
-        return "physiotherapists/update";
+        return "physios/update";
     }
 
     @DeleteMapping("/{id}")
@@ -42,8 +55,10 @@ public class PhysiotherapistsController {
         physiotherapistService.delete(id);
     }
 
-    @GetMapping
-    public List<Physiotherapist> getPhysiotherapists() {
-        return physiotherapistService.list();
+    @GetMapping("/")
+    public String list(Model model) {
+        List<Physiotherapist> physiotherapists = physiotherapistService.list();
+        model.addAttribute("physiotherapists", physiotherapists);
+        return "/views/physios/list";
     }
 }
